7cfbbe4c1ef9daa5a01e34958b3e1685012592c5
bruno pessoa nunes de melo <brunopessoa.melo@gmail.com>
2021-09-15 12:57:35 -0300

    testando os layouts e a rota

---
+++ movie-app/resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale1">
+        <title>Movie Appp</title>
+
+        <link rel="stylesheet" href="/css/main.css">
+    </head>
+<body>
+    <nav>Nav goes here</nav>
+    @yield('content')
+</body>
+</html>
